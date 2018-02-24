ed42a97a79cac19050da56311da772c0da815420
LeesimLoveXz <565048350@qq.com>
2018-02-24 18:00:29 +0800

    Initial Commit

---
+++ NSTimerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSTimerDemo
+//
+//  Created by Leesim on 2018/2/24.
+//  Copyright © 2018年 LiMing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NSTimerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSTimerDemo
+//
+//  Created by Leesim on 2018/2/24.
+//  Copyright © 2018年 LiMing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
